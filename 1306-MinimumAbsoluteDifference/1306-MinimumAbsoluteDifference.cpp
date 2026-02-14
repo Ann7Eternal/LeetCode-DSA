69ccca55ac8587cfd56054ff33b9dff3938a70e5
Yashaswi Harsh <144933623+Ann7Eternal@users.noreply.github.com>
2026-02-14 13:41:16 +0530

    Create 1306-MinimumAbsoluteDifference

---
+++ 1306-MinimumAbsoluteDifference/1306-MinimumAbsoluteDifference.cpp
@@ -0,0 +1,26 @@
+// Last updated: 2/14/2026, 1:41:15 PM
+class Solution {
+public:
+    vector<vector<int>> minimumAbsDifference(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+
+        vector<vector<int>> result;
+        int minDiff = INT_MAX;
+
+        for (int i = 0; i < arr.size() - 1; i++) {
+            
+            int currentDiff = arr[i+1] - arr[i];
+
+            if (currentDiff < minDiff) {
+                minDiff = currentDiff;
+                result.clear(); 
+                result.push_back({arr[i], arr[i+1]});
+            }
+            else if (currentDiff == minDiff) {
+                result.push_back({arr[i], arr[i+1]});
+            }
+        }
+
+        return result;
+    }
+};
