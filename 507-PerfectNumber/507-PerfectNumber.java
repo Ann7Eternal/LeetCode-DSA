1c779648e1bc136694a50ba088d475eb29c4b3b8
Yashaswi Harsh <144933623+Ann7Eternal@users.noreply.github.com>
2026-02-14 13:41:31 +0530

    Create 507-PerfectNumber

---
+++ 507-PerfectNumber/507-PerfectNumber.java
@@ -0,0 +1,18 @@
+// Last updated: 2/14/2026, 1:41:30 PM
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int num1 = 0;
+        if(num % 2 != 0){
+            return false;
+        }else{
+        
+        for(int i  = 1 ; i<=num/2 ; i++){
+            if(num % i == 0){
+                num1 += i; 
+            }
+        }
+        }
+
+        return num1==num;
+    }
+}
