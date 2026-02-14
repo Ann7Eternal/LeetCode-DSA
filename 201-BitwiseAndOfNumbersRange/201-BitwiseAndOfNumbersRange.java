e1e3bd470c9a2e302ec8adbd93b3e941b385c6e1
Yashaswi Harsh <144933623+Ann7Eternal@users.noreply.github.com>
2026-02-14 13:41:40 +0530

    Create 201-BitwiseAndOfNumbersRange

---
+++ 201-BitwiseAndOfNumbersRange/201-BitwiseAndOfNumbersRange.java
@@ -0,0 +1,12 @@
+// Last updated: 2/14/2026, 1:41:39 PM
+class Solution {
+    public int rangeBitwiseAnd(int left, int right) {
+        int cnt = 0;
+        while (left != right) {
+            left >>= 1;
+            right >>= 1;
+            cnt++;
+        }
+        return (left << cnt);
+    }
+}
